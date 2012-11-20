b14efff504619da7bcf93bb2c7c78f63edcd5d3e
Florent Garnier <florent.garnier@gmail.com>
2012-11-20 18:04:00 +0100

    adding needed interfaces files for trace

---
+++ trace.mli
@@ -0,0 +1,3 @@
+val print_sys_control : Trace_types.sys_control -> string
+val print_trace_l_folder : string -> Trace_types.sys_control -> string
+val print_trace : Trace_types.sys_control list -> string
---
+++ trace_types.mli
@@ -0,0 +1,2 @@
+type sys_control = Sys_control of string * string
+type trace = sys_control list
