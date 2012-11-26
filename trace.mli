f5634e03bed367bae7a3db7ebdf44a530815650f
Florent Garnier <florent.garnier@gmail.com>
2012-11-26 16:00:28 +0100

    adding interface file for trace

--- trace.mli
+++ trace.mli
@@ -1,3 +1,15 @@
 val print_sys_control : Trace_types.sys_control -> string
 val print_trace_l_folder : string -> Trace_types.sys_control -> string
 val print_trace : Trace_types.sys_control list -> string
+val pprint_esid : Trace_types.esid -> string
+val pprint_sid : Trace_types.sid -> string
+val pprint_folder_esid_sid_map :
+  Trace_types.esid -> Trace_types.sid -> string -> string
+val pprint_esidsid_map :
+  (Trace_types.esid, Trace_types.sid) Hashtbl.t -> string
+val pprint_folder_sid_code_map :
+  Trace_types.sid -> string -> string -> string
+val pprint_sid_to_code_info : (Trace_types.sid, string) Hashtbl.t -> string
+val pprint_map_2_fcinfo : Trace_types.map_2_fcinfos -> string
+val pprint_tr_subsystem_table :
+  ('a, Trace_types.map_2_fcinfos) Hashtbl.t -> string
